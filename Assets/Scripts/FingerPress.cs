9b71e619acf6c180d594ff64c5b13411b38c0526
hchangjae <hcj@payw.org>
2020-01-28 19:30:34 +0900

    make new file

---
+++ Assets/Scripts/FingerPress.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private double randomFallTime;
+    double timer;
+    double fps;
+    bool resetFallTime;
+    public bool fall;
+    public bool ispressed = false;
+
+    void Start()
+    {
+        fall = false;
+    }
+
+    void Update()
+    {
+        if (!ispressed)
+        {
+
+            fall = false;
+            return;
+        }
+       
+        timer += Time.deltaTime;
+
+
+        if(timer > randomFallTime)
+        {
+            fall = true;
+        }
+
+        // DO SOMETHING HERE
+        Debug.Log("Pressed");
+        Debug.Log("elapsed time = " + timer);
+        Debug.Log("fall time = " + randomFallTime);
+
+
+    }
+    
+    public void OnMouseDown()
+    {
+        if (!ispressed)
+        {
+            ispressed = true;
+            randomFallTime = Random.Range(4.0f, 10.0f);
+        }
+        else
+        {
+            
+        }
+    }
+    public void OnMouseUp()
+    {
+        timer = 0;
+        ispressed = false;
+    }
+}
---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public FingerPress fingerscript;
+    public int stage;
+    public GameObject finbtn;
+
+    private Transform target;
+    float step;
+    int ratio;
+    Vector3 originalPos;
+
+    void Start()
+    {
+        originalPos = new Vector3(1, 7, 0);
+        fingerscript = GameObject.Find("FingerPressBtn").GetComponent<FingerPress>();
+        ratio = 10;    
+        stage = 10;
+        step = Time.deltaTime * stage * ratio;
+        finbtn = GameObject.Find("FingerPressBtn");
+        target = finbtn.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        if(fingerscript.fall)
+        {
+            Debug.Log("falllllllll!!!!!!!!");
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+        
+        if(fingerscript.ispressed && transform.position.y < -4.2)
+        {
+            Debug.Log("gamever");
+        }
+
+        if(!fingerscript.ispressed)
+        {
+            transform.position = originalPos;
+        }
+
+
+
+    }
+}
