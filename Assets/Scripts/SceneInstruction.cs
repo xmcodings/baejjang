4a6e2d7b6af463c045434684bcdb079cc2532eec
d0o0yle <doili0552@gmail.com>
2020-01-31 16:46:13 +0900

    add main menu

---
+++ Assets/Scripts/SceneInstruction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneInstruction : MonoBehaviour
+{
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    void Start()
+    {
+
+    }
+
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene("InstructionScene", LoadSceneMode.Single);
+
+    }
+
+}
---
+++ Assets/Scripts/SceneMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneMainMenu : MonoBehaviour
+{
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    void Start()
+    {
+
+    }
+
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+
+    }
+
+}
